3d17e65dd57f6b3470a4a7aa7593564e371fb31b
Júlia Vieira <juliavieira8390@gmail.com>
2022-01-11 18:25:40 -0300

    Mais duas mensagens

--- src/application/Program.java
+++ src/application/Program.java
@@ -4,6 +4,8 @@ public class Program {
 
 	public static void main(String[] args) {
 		System.out.println("Olá, Mundo");
+		System.out.println("Olá, pessoas");
+		System.out.println("Olá, meninos!");
 	}
 
 }
